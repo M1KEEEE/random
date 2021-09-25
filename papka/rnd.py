a20ebd790ef05cfa17d9fd248af9610d14319f3d
M1KEEEE <72807912+M1KEEEE@users.noreply.github.com>
2021-09-25 13:01:00 +0300

    Add files via upload

--- papka/rnd.py
+++ papka/rnd.py
@@ -1,1 +1,11 @@
+import random 
 
+counter = 0
+my_rnd = 0
+
+while not my_rnd == 100:
+    my_rnd = random.randrange(101)
+    counter += 1
+    if my_rnd == 100:
+        print("Выпало 100, это заняло", counter, "итераций")
+		
